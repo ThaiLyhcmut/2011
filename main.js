5df4764804515197e7a6c6d23589092f25fbac65
Thaily <lyvinhthai321@gmail.com>
2024-11-21 18:17:51 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+// onload = () =>{
+//         document.body.classList.remove("container");
+// };
 // set up text to print, each item in array is new line
